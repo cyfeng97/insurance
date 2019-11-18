1187bb510a63a3ba8d4275dea859993397f6b62d
anreh <anurehka@gmail.com>
2019-11-18 13:06:20 +0800

    calculate

--- app/src/main/java/com/example/insurance/MainActivity.kt
+++ app/src/main/java/com/example/insurance/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener,
     AdapterView.OnItemSelectedListener {
@@ -37,14 +38,41 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener,
     private fun calculatePremium() {
         val position = spinnerAge.selectedItemPosition
         val gender = radioGroupGender.checkedRadioButtonId
+        var basic_premium: Int = 0
+        val symbol = Currency.getInstance(Locale.getDefault()).symbol
+
+        basic_premium = when(position){
+            0 -> 60 //less than 17
+            1 -> 70 //17 to 25
+            2 -> 90 //26-30
+            3 -> 12 //31-40
+            else -> 150 // 41-55/more than 55
+        }
 
         if (gender == R.id.radioButtonMale) {
             // TODO clculate premium for male
+            basic_premium += when(position){
+                0 -> 0
+                1 -> 50
+                2 -> 100
+                3 -> 150
+                else -> 200
+            }
+
         }
 
         if (checkBoxYes.isChecked){
             // TODO calculate premium for smoker
+            basic_premium += when(position){
+                0 -> 0
+                1 -> 100
+                2 -> 150
+                3 -> 200
+                4 -> 250
+                else -> 300
+            }
         }
+        textViewPremium.text = String.format("%s : %s %d",getString(R.string.insurance_premium),symbol,basic_premium)
     }
 
 }
